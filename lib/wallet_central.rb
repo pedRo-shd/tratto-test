ab189bc2e7aaf5398ba42b2309f2da12dff6b264
Pedro Custódio <pedro@dogme.com.br>
2018-04-26 13:53:35 -0300

    Find client through params name

--- lib/wallet_central.rb
+++ lib/wallet_central.rb
@@ -4,6 +4,15 @@ class WalletCentral
 
   def self.output(name=nil)
     build_clients = BuildClients.new.load_clients
-    build_clients.map(&:build_output)
+    return search_client(build_clients, name) if name
+    clients_and_wallets(build_clients)
+  end
+
+  def self.search_client(data_clients, name)
+    clients_and_wallets(data_clients).find { |user| user[:name] == name.to_s }
+  end
+
+  def self.clients_and_wallets(data_clients)
+    data_clients.map(&:build_output)
   end
 end
--- spec/wallet_spec.rb
+++ spec/wallet_spec.rb
@@ -5,11 +5,26 @@ RSpec.describe WalletCentral do
 
   describe ".output" do
     before do
-      @wallet_central = WalletCentral.output
+      @wallet_central   = WalletCentral.output
+      @set_client_jon   = WalletCentral.output('jon')
+      @set_client_sansa = WalletCentral.output('sansa')
     end
 
-    it "must return all clients" do
-      expect(@wallet_central.count).to eql(11)
+    context "when set all clients" do
+      it "must return all clients" do
+        expect(@wallet_central.count).to eql(11)
+      end
     end
+
+    context "when search one client" do
+      it "must return client jon" do
+        expect(@set_client).to eql('jon')
+      end
+
+      it "must return client sansa" do
+        expect(@set_client_jon[0][:name]).to eql('sansa')
+      end
+    end
+
   end
 end
