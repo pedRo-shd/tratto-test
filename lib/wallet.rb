8a53cfa579ed7666cb3c8828e7f45fb5ae5b4873
Pedro Custódio <pedro@dogme.com.br>
2018-04-26 17:50:50 -0300

    Adds and substracts values about the transfer through amounts from and to users

--- lib/wallet.rb
+++ lib/wallet.rb
@@ -1,23 +1,31 @@
 require 'ostruct'
 require_relative 'client'
 
-class Wallet
+class Wallet < OpenStruct
   def initialize(currency, amount, client=nil, obj_array=nil)
-    @source = OpenStruct.new(currency: currency, amount: amount)
+    super(currency: currency, amount: amount)
     @obj_array = obj_array
     @client = client
   end
 
   def build_client
     clients = @obj_array.find{|client| client.name == @client }
-    return Client.new(@client, [@source]) unless clients
+    return Client.new(@client, [self]) unless clients
     build_array(clients)
+  end
+
+  def substract(value)
+    self.amount -= value
+  end
+
+  def add(value)
+    self.amount += value
   end
 
   private
 
   def build_array(clients)
-    clients.wallets << @source
+    clients.wallets << self
     return
   end
 end
