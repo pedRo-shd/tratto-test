0bbb113e59fff012a611cd160a9925c062837dfe
Pedro Custódio <pedro@dogme.com.br>
2018-04-26 17:46:36 -0300

    Must transfer value to another user

--- spec/wallet_spec.rb
+++ spec/wallet_spec.rb
@@ -18,17 +18,32 @@ RSpec.describe WalletCentral do
 
     context "when search client" do
       it "must return client jon" do
-        expect(@set_client_jon[:name]).to eql('jon')
+        expect(@set_client_jon).to eql({:name=>"jon", :wallets=>{"EUR"=>868.65, "USD"=>463.39}})
       end
 
       it "must return client sansa" do
-        expect(@set_client_sansa[:name]).to eql('sansa')
+        expect(@set_client_sansa).to eql({:name=>"sansa", :wallets=>{"EUR"=>1065.45, "BRL"=>586.28}})
       end
 
       it "mustn't return client that not exists" do
         expect { WalletCentral.output('elliot') }.to raise_error('elliot is not a client')
       end
     end
-
   end
+
+  describe ".transfer" do
+    before do
+      @transfer1 = WalletCentral.transfer('jon', 'tyrion', 'USD', 100)
+    end
+
+    context "when are same currency" do
+      it "must add to client amount" do
+        expect(@set_client_jon).to eql({:name=>"tyrion", :wallets=>{"EUR"=>868.65, "USD"=>1068.12}})
+      end
+
+      # it "must substract to client amount" do
+      # end
+    end
+  end
+
 end
