6d5943505d67f3dcc823e964a6f04b3ef88f3e1c
Pedro Custódio <pedro@dogme.com.br>
2018-04-27 14:27:10 -0300

    Adjusting

--- lib/transaction_operation.rb
+++ lib/transaction_operation.rb
@@ -17,6 +17,7 @@ class TransactionOperation
 
   def find_wallets(client_origin_or_destination)
     currency_wallet = client_origin_or_destination.wallets.find { |w| w.currency == @currency.to_s }
+    apply_exceptions(client_origin_or_destination.name, currency_wallet)
     return convert_amount unless currency_wallet
     currency_wallet
   end
@@ -36,6 +37,11 @@ class TransactionOperation
     convert_currency = ConvertCurrency.new(@currency, to_currency.currency, @value)
     @value = convert_currency.convert
     to_currency
+  end
+
+  def apply_exceptions(client_origin, currency_wallet)
+    raise Exception, "You don't have the #{@currency} wallet you tried to transfer" if client_origin == @from_client.name && currency_wallet.nil?
+    raise Exception, "Insufficient ballance to transfer" if client_origin == @from_client.name && @value > currency_wallet.amount
   end
 
 end
